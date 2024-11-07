1a71dbfd257760af8524a5bbed9a22451b0e657f
AnggerMaulana <anggerdoang74@gmail.com>
2024-11-07 11:19:19 +0700

    angger & faqih

--- register.php
+++ register.php
@@ -1,92 +1,88 @@
 <?php
-include "koneksi.php";
+require 'fungsi/koneksi.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>registrasi ke Perpustakaan Digital</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="aset/css/styles.css">
+    <title>Register</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body class="bg-light">
 
-<div class="container">
-    <div class="row justify-content-center" style="height: 100vh;">
-        <div class="col-md-4">
-            <div class="card mt-5">
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-6 mx-auto">
+            <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title text-center">Daftar akun</h5>
+                    <h3 class="text-center">Register</h3>
                     <?php
-                         if( isset($_POST['register'])) {
-                             $nama = $_POST['nama'];
-                             $email = $_POST['email'];
-                             $alamat = $_POST['alamat'];
-                             $no_telepon = $_POST['no_telepon'];
-                             $username = $_POST['username'];
-                             $level = $_POST['level'];
-                             $password = md5($_POST['password']);
+                        if( isset($_POST['register'])) {
+                            $nama = $_POST['nama'];
+                            $email = $_POST['email'];
+                            $alamat = $_POST['alamat'];
+                            $no_telepon = $_POST['no_telepon'];
+                            $username = $_POST['username'];
+                            $level = $_POST['level'];
+                            $password = md5($_POST['password']);
 
-                             $insert = mysqli_query($koneksi, "INSERT INTO user(nama,email,alamat,no_telepon,username,password,level) VALUES('$nama','$email','$alamat','$no_telepon','$username','$password','$level')"); 
+                            $insert = mysqli_query($koneksi, "INSERT INTO user(nama,email,alamat,no_telepon,username,password,level) VALUES('$nama','$email','$alamat','$no_telepon','$username','$password','$level')"); 
                         
-                             if($insert) {
-                                 echo '<script>alert("Pendaftaran Berhasil!"); location.href="login.php"<script>';   
-                             }else{
-                                 echo '<script>alert("Pendaftaran Gagal!");<script>';   
-                             }
-                         }
+                            if($insert) {
+                                echo '<script>alert("Pendaftaran Berhasil!"); location.href="login.php"</script>';   
+                            }else{
+                                echo '<script>alert("Pendaftaran Gagal!");</script>';   
+                            }
+                        }
 
                             
                     ?>
                     <form method="post">
-                        <div class="form-group">
-                            <label class="small mb-1">Nama Lengkap</label>
+                        <div class="mb-3">
+                            <label for="fullName" class="form-label">Nama Lengkap</label>
+                            <input type="text" class="form-control" name="nama" required placeholder="masukkaan nama lengkap anda...">
                         </div>
-                        <div class="form-group">
-                        <label class="small mb-1">Nama Lengkap</label>
-                            <input type="email" required class="form-control" name="email" placeholder="Email..." />
+                        <div class="mb-3">
+                            <label for="registerEmail" class="form-label">Email address</label>
+                            <input type="email" class="form-control" name="email" required placeholder="Email...">
                         </div>
-                        <div class="form-group">
-                            <label class="small mb-1">No Telepon</label>
-                            <input type="no_telepon" required class="form-control" name="no_telepon" placeholder="No Telepon..." />
+                        <div class="mb-3">
+                            <label for="phone" class="form-label">No Telepon</label>
+                            <input type="number" class="form-control" name="no_telepon" required placeholder="No Telepon...">
                         </div>
-                        <div class="form-group">
-                            <label class="small mb-1">Alamat</label>
-                            <textarea name="alamat" required class="form-control" placeholder="alamat..."></textarea>
+                        <div class="mb-3">
+                            <label for="address" class="form-label">Alamat</label>
+                            <textarea type="text" class="form-control" name="alamat" rows="5" placeholder="Alamat Lengkap..."></textarea>
                         </div>
-                        <div class="form-group">
-                            <label class="small mb-1">Username</label>
-                            <input type="no_telepon" required class="form-control" name="username" placeholder="username..." />
+                        <div class="mb-3">
+                            <label for="registerPassword" class="form-label">Username</label>
+                            <input type="text" class="form-control" name="username" required placeholder="Username...">
                         </div>
-                        <div class="form-group">
-                            <label class="form-label">Password</label>
-                            <input type="nama" required class="form-control" name="password" placeholder="password..." />
+                        <div class="mb-3">
+                            <label for="registerPassword" class="form-label">Password</label>
+                            <input type="password" class="form-control" name="password" required placeholder="Password...">
                         </div>
-                        <div class="form-group">
-                            <label class="form-label">Level</label>
-                            <input type="nama" required class="form-control" name="level" placeholder="level..." />
-                        </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="password" class="form-label">Daftar Sebagai</label>
-                            <select class="form-select" aria-label="Default select example">
-                                 <option value="admin">admin</option>
-                                 <option value="peminjam">peminjam</option>
+                        <div class="mb-3">
+                            <label for="userRole" class="form-label">Role</label>
+                            <select class="form-select" name="level" required>
+                                <option value="" disabled selected>Pilih role</option>
+                                <option value="admin">Admin</option>
+                                <option value="petugas">Petugas</option>
+                                <option value="peminjam">Peminjam</option>
                             </select>
                         </div>
-                        <button type="submit" name="register" value="register" class="btn btn-primary w-100">Daftar</button>
+                        <button type="submit" name="register" value="register" class="btn btn-primary w-100">Register</button>
                     </form>
-                    <div class="text-center mt-3">
-                        <p>Sudah punya akun?<a href="login.php">Login</a></p>
-                    </div>
+                    <p class="mt-3 text-center">Already have an account? <a href="login.php">Login</a></p>
                 </div>
             </div>
         </div>
     </div>
 </div>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
-<script src="aset/js/login.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+<script src="js/g.js"></script>
 </body>
 </html>
